23abc3429424c472aeeafe44a6a940d535731cb3
Christophe Hesters <toefel18@gmail.com>
2019-10-29 11:51:45 +0100

    JIT benchmark

---
+++ src/main/java/nl/toefel/JITBenchmark.java
@@ -0,0 +1,30 @@
+package nl.toefel;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class JITBenchmark {
+
+    public class IdGenerator {
+        private long seed = System.nanoTime();
+
+        public long getId() {
+            return seed;
+        }
+    }
+
+    @Benchmark
+    public long scalarReplacementNoAllocationBenchmark() {
+        IdGenerator idGenerator = new IdGenerator();
+        return idGenerator.getId();
+    }
+
+}
+
